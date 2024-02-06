74b3bf57cd0c746a07a4da759520b896033b4bca
Dimitrije Radojević <me@dimitrije.website>
2024-02-06 09:15:04 +0100

    Pin nixpkgs-21.11.

--- shell.nix
+++ shell.nix
@@ -1,13 +1,14 @@
 let
   sources = import ./nix/sources.nix;
   pkgs = import sources.nixpkgs { };
-  lexicon = ps: ps.callPackage ./lexicon.nix {};
+  lexicon = ps: ps.callPackage ./lexicon.nix { };
   python = pkgs.python3.withPackages (p: with p; [
     certbot
     acme
     mock
     pynamecheap
     (lexicon p)
+    pip
   ]);
 in
 python.env
